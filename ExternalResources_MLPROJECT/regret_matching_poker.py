161a2215f7c536b2577030b3c469f378183e2e2d
garyjh126 <garyjh126@gmail.com>
2018-10-30 17:42:29 +0000

    Designing Action interface

--- ExternalResources_MLPROJECT/regret_matching_poker.py
+++ ExternalResources_MLPROJECT/regret_matching_poker.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import os
 import uuid
+from abc import abstractmethod, ABCMeta
+
 
 
 '''
@@ -30,51 +32,51 @@ class Table(Game):
 
 
     num_of_players = 0
+    
     def __init__(self, player_list, positions_at_table):
-        for i in range(len(player_list)):
-            self.player_list[i] = player_list[i]
-            #self.position_list.append(player_list[i].position)
+        self.player_list= player_list.copy()
+        for i in player_list:
             num_of_players += 1
         self.positions_at_table = positions_at_table.copy()
 
-    def get_players_at_table():
+    def get_players_at_table(self):
         return self.player_list
 
-    def get_player_at_position(position):
+    def get_player_at_position(self, position):
         for i in self.player_list:
             if i.position == position:
                 return i
     
-    def get_player_by_ID(ID):
-        for player in player_list:
+    def get_player_by_ID(self, ID):
+        for player in self.player_list:
             if player.ID == ID: 
                 return player
 
-    def get_position_of_player(player):
+    def get_position_of_player(self, player):
         for i in self.player_list:
             if i == player:
                 return i.position
 
-    def get_number_of_players():
-        return num_of_players
+    def get_number_of_players(self):
+        return self.num_of_players
 
-    def rotate():
+    def rotate(self):
         keys = self.positions_at_table.keys()
         values = self.positions_at_table.values()
         shifted_values = values.insert(0, values.pop())
         new_positions_at_table = dict(zip(keys, shifted_values))
         self.positions_at_table = new_positions_at_table
 
-    def remove_player(player):
+    def remove_player(self, player):
         for i in self.player_list:
             if i == player:
-                list_of_players.remove(player)
-        for index, pos in positions_at_table.items():
+                self.player_list.remove(player)
+        for index, pos in self.positions_at_table.items():
             if pos == player.position:
-                del positions_at_table[pos]
-                reinstantiate_positions_at_table(pos)
+                del self.positions_at_table[pos]
+                self.reinstantiate_positions_at_table(pos)
 
-    def reinstantiate_positions_at_table(player_to_remove):
+    def reinstantiate_positions_at_table(self, player_to_remove):
         keys = []
         for index in range(len(self.positions_at_table)):
             keys.append(index)
@@ -84,7 +86,9 @@ class Table(Game):
 
 
 class Player(Game):
-    def __init__(self, ID, name, card_holding, position):
+
+
+    def __init__(self, ID, name, card_holding, position, stack_size = 50):
         self.ID = ID
         self.name = name
         self.card_holding = card_holding
@@ -92,14 +96,52 @@ class Player(Game):
         self.strategy, self.avg_strategy,\
         self.strategy_sum, self.regret_sum = np.zeros((4, 3))
         self.list_of_actions_game = np.array([])
+        self.stack_size = stack_size 
+        self.action = ''
 
     def __str__(self):
         return self.name
 
-    def send_actions_to_game
+    def take_action(self):
+        pass
 
+
+#INTERFACE
+class Action(Player):
+
+    __metaclass_ = ABCMeta 
+
+  
+    @abstractmethod
+    def determine_action(self): pass
+    
+    @abstractmethod
+    def determine_table_stats(self): pass
+        
+    @abstractmethod
+    def send_file(self): pass
+        
+    @abstractmethod
+    def get_action_of_preceding_player(self): pass
+        
+    @abstractmethod
+    def populate_regret_table(self): pass
+        
+
+class Bet(Action):
+    
+    def __init__(self, amount):
+        self.amount = amount
 
     
+
+class Call(Action):
+    pass
+
+class Fold(Action):
+    pass
+
+
 
 class PokerRound(Table):
 
@@ -110,16 +152,19 @@ class PokerRound(Table):
         self.list_of_actions_player2 = []
 
 
-
+class CardHolding(Player):
+    pass
 
 
 
 
 if __name__ == '__main__':
-    os.remove("strategy_stats.txt")
+    
     game = Game()
-    print('==== Use simple regret-matching strategy === ')
-    game.play()
-    print('==== Use averaged regret-matching strategy === ')
-    game.conclude()
-    game.play(avg_regret_matching=True)
+
+    #os.remove("strategy_stats.txt")
+    #print('==== Use simple regret-matching strategy === ')
+    #game.play()
+    #print('==== Use averaged regret-matching strategy === ')
+    #game.conclude()
+    #game.play(avg_regret_matching=True)
